b2af171cf43fa06727bc01036a49684eeb12a630
VISHAL S <svishal27092004@gmail.com>
2025-02-02 20:09:24 +0530

    Age Group Classification Program.
    This Java program classifies a person's age into different groups based on user input. It checks if the age falls within predefined ranges and prints "Child", "Teenager", "Adult", or "Senior" accordingly. If the input is negative, it displays an "Invalid age input" message. The program uses simple conditional statements and ensures proper user interaction through console input.

---
+++ AgeGroupClassifier.java
@@ -0,0 +1,22 @@
+package com.studyopedia;
+import java.util.Scanner;
+public class AgeGroupClassifier {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        if (age >= 0 && age <= 12) {
+            System.out.println("Child");
+        } else if (age >= 13 && age <= 19) {
+            System.out.println("Teenager");
+        } else if (age >= 20 && age <= 64) {
+            System.out.println("Adult");
+        } else if (age >= 65) {
+            System.out.println("Senior");
+        } else {
+            System.out.println("Invalid age input.");
+        }
+        sc.close();
+    }
+}
+
